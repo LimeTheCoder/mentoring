cba139c6c9073f836cd1ecbd3fadf5fd33d1aa2d
Taras Sakharchuk <limethecoder@gmail.com>
2018-01-10 19:57:24 +0200

    Add service discovery

--- ClassManagement/src/main/java/com/limethecoder/Application.java
+++ ClassManagement/src/main/java/com/limethecoder/Application.java
@@ -1,13 +1,12 @@
 package com.limethecoder;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- RegistryService/src/main/java/com/limethecoder/EurekaServiceApplication.java
+++ RegistryService/src/main/java/com/limethecoder/EurekaServiceApplication.java
@@ -2,12 +2,13 @@ package com.limethecoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-@EnableDiscoveryClient
-public class Application {
+public class EurekaServiceApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(EurekaServiceApplication.class, args);
     }
 }
